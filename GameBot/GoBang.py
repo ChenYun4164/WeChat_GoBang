7c1464a30eb1ea1fd3d1c2643ea612bfe95c6270
ChenYun <752914836@qq.com>
2020-05-20 17:11:33 +0800

    第一次提交

--- GameBot/GoBang.py
+++ GameBot/GoBang.py
@@ -1,49 +1,3 @@
->    苦练技术是不可能的，这辈子都练不会，学技术又太枯燥，就只有靠辅助才能维持的生活这样子。
-
-[TOC]
-
-#### 简述
-
-需要的工具主要分为以下三类：
-
-1.   <a href = “https://www.aiexp.info/pages/yixin-cn.html "> Yixin 奕心引擎</a>，这个引擎是国人所作，可以说是非商用版里面最强的五子棋AI
-
-     一开始我想使用奕心的界面+引擎那款，因为可定制性足够强，结果发现Python 不好与GUI程序交互，所以就选择了引擎。然而尴尬的一点是，把官方文档翻了个遍都没有找到引擎的使用方法。不过后来在<a href="https://gomocup.org/"> 世界五子棋锦标赛</a>  找到了参加比赛的AI 必备的两种接口。
-
-     -    http://petr.lastovicka.sweb.cz/protocl2en.htm 使用输入输出流，本文选择的就是这种方式
-
-     -    http://petr.lastovicka.sweb.cz/protocl1en.htm 使用文件
-
-2.   Python 简单图片处理
-
-3.   Python adb 操作手机 
-
-
-
-#### 思路
-
-<img src="https://img2020.cnblogs.com/blog/1512048/202005/1512048-20200520161002204-2008792351.png" alt="image-20200329134722139" style="zoom: 67%;" />
-
-整个思路顺下来程序其实是比较好写的，就是前期**需要手动的截取匹配图片，设置查找区域像素点位置**比较麻烦，不过新添加自适配功能，这些都不需要做。
-
-
-
-#### 实现
-
-##### 前期准备
-
-你需要预先掌握的知识：
-
-1.   会运行python 文件，并且安装文中的库
-2.   <a href="https://jingyan.baidu.com/article/22fe7cedf67e353002617f25.html">安装Adb</a> 并 <a href="https://github.com/mzlogin/awesome-adb#%E6%97%A0%E7%BA%BF%E8%BF%9E%E6%8E%A5%E9%9C%80%E8%A6%81%E5%80%9F%E5%8A%A9-usb-%E7%BA%BF">无线连接成功</a> 
-3.   在开发者选项里，打开 USB 调试 和 USB允许模拟点击
-4.   修改mVars 里的address 和 ip 
-
-
-
-这个是需要进行适配的参数，除前两项address 和 ip 外，其它在分辨率适配正常的情况下都不需要更改
-
-```c++
 class mVars:
     address='C:/Users/EA/Desktop/yixin/'    # 使用到的文件所存放地址
     ip = '10.224.133.140:5555'              #ip 地址，详见adb模块中adb连接教程
@@ -54,17 +8,9 @@ class mVars:
     confirmWin = (660,1780,660+46,1780+46)  #用来确定是否胜利的区域
     chickBack = (100,1820)                  #胜利后 返回图标 的位置
     chickBegin = (540,940)                  #开始匹配 的位置
-```
 
 
 
-##### Yixin 引擎
-
-从<a href = “http://gomocup.org/static/download-ai/YIXIN18.zip">官网</a>上可以下载Yixin 引擎,结合 <a href = “http://petr.lastovicka.sweb.cz/protocl2en.htm">协议</a>  可以知道如何与引擎交互
-
-使用 subprocess 让 python 与 引擎交互
-
-```python
 import subprocess as sub
 class YiXin:
     mYixin = sub.Popen(mVars.address+"Yixin.exe", stdin=sub.PIPE, stdout=sub.PIPE, stderr=sub.PIPE)
@@ -100,22 +46,8 @@ class YiXin:
         self.input('RESTART 15')
         self.output()
 
-```
 
 
-
-##### 图片处理
-
-这个模块需要做的事就是处理跟图片相关的，包括比较图片，转换坐标等
-
-关于识别对手落子位置，有两种实现思路：
-
-1.   每隔一段时间获取截一张图，对比两张图不同的地方，从而获取对手落点位置。
-2.   每隔一段时间截一张图，识别图上的所有有棋的位置并保存，然后通过比较，得到对手落子位置。
-
-目前只实现第一种方法，不过使用第二种方法可以从半局开始。
-
-```python
 import aircv as ac
 class ImageProcess:
     #缩放检测的图片
@@ -174,15 +106,9 @@ class ImageProcess:
     def difference(self,img1,img2):
         return img1-img2
 
-```
 
 
 
-##### ADB 模块
-
-这个模块与手机交互，用的是无线ADB连接，见 <a href="https://github.com/mzlogin/awesome-adb">ADB连接教程</a> 
-
-```python
 import os
 import time
 class Adb:
@@ -210,15 +136,9 @@ class Adb:
         time.sleep(0.1)
         os.system('adb shell input tap %d %d'%(piexl[0],piexl[1]))
 
-```
+ 
 
 
-
-##### 游戏系统模块
-
-衔接各个模块
-
-```python
 class System:
     Yixin = YiXin() 
     ImageP = ImageProcess()
@@ -226,13 +146,14 @@ class System:
     imgobj = None   #用来检测对手落子的图片
     certain = 0     #0表示未确认，1表示己方为白，2表示己方为黑
     
+
     #按手机像素适配
     def __init__(self):
         piexl = self.Adb.resolution()
         zoom = (piexl[0]/1080,piexl[1]/1920)
         self.zoomVars(zoom)
         self.ImageP.zoomImage(zoom)
-        
+   
     #缩放像素参数
     def zoomVars(self,zoom):
         mVars.boradOne = int(mVars.boradOne*zoom[0])
@@ -332,13 +253,8 @@ class System:
         self.certain = 0
         self.imgobj = None
 
-```
 
 
-
-##### 主函数
-
-```python
 if __name__ == "__main__":
     msys = System()
 
@@ -350,66 +266,3 @@ if __name__ == "__main__":
         print("You Win !! Next Game Will Begin After 4sec")
         time.sleep(4)
         msys.newGame()    
-```
-
-
-
-#### 其它
-
-如果想自定义的话，可以按照以下教程
-
->    在已连接adb 的情况下，可以通过下面这条语句快速截图
->
->    ```python
->    import os
->    address='C:/Users/EA/Desktop/yixin/'
->    os.system('adb exec-out screencap -p > '+address+'sc.jpg')
->    ```
-
-主要用系统自带的画图工具，在屏幕左下角查找相应位置的像素坐标
-
--    检测己方是黑白棋
-
-     <img src="https://img2020.cnblogs.com/blog/1512048/202005/1512048-20200520161003410-1386067403.png" alt="image-20200401170446037" style="zoom:50%;" />
-
-     建议用下面的命令获取
-
-     ```python
-     import aircv as ac
-     address='C:/Users/EA/Desktop/yixin/'
-     
-     imgsrc = ac.imread(address+'sc.jpg')
-     x0,y0,x1,y1 = (820,1590,820+45,1590+60) #这个坐标以用画图工具量取为准
-     imgsrc = imgsrc[y0:y1,x0:x1]
-     
-     #分别在这个位置保存己方黑白棋识别标志
-     ac.cv2.imwrite(address+'confirmw.jpg',imgsrc)
-     #ac.cv2.imwrite(address+'confirmb.jpg',imgsrc)
-     
-     #检测所选的图片是否正常识别
-     imgw = ac.imread(address+'confirmw.jpg')
-     print(ac.find_template(imgsrc,imgw,0.01))
-     ```
-
-     注意，1. 不要把整个棋子都截取，因为右边不时有绿色关环影响识别。2. 与电脑对战时这个标志的位置偏右，所以在mVars.confirmBW 的范围应该稍大一点。3. 在弄完之后，最好识别一下检测一下。
-
--    胜利的检测，我选的是这个
-
-     <img src="https://img2020.cnblogs.com/blog/1512048/202005/1512048-20200520161004343-226297140.png" alt="image-20200401171752084" style="zoom:50%;" />
-
--    空棋盘，就弄张空棋盘的截图，这个图是必要的，如果没有，当己方执白时，对方第一子下的很快还没来得及截图，那么系统就检测不到这个对方落子。
-
--    检测对方落子简单起见，我是直接两张图片相减然后直接截图保存。
-
-     <img src="https://img2020.cnblogs.com/blog/1512048/202005/1512048-20200520161005181-963416149.png" alt="image-20200401172652696" style="zoom: 50%;" />
-
-     ```
-     img1 = ac.imread(address+'old.jpg')
-     img2 = ac.imread(address+'new.jpg')
-     ac.cv2.imwrite(address+'diff.jpg',img1-img2)
-     ```
-
-     需要注意的是，一定要统一顺序，旧减新或新减旧，我的实现是用旧减新，新减旧也可以，只是棋子的图不一样，下面就是新减旧
-
-     <img src="https://img2020.cnblogs.com/blog/1512048/202005/1512048-20200520161005824-2030586172.png" alt="image-20200401173039933" style="zoom:50%;" />
-
